1c9e73cbfd02eece6514a622f58584fb9f90325c
Vufysay <max03082009@mail.ru>
2024-05-31 00:12:39 +0300

    first commit

---
+++ app/api/config.js
@@ -0,0 +1,6 @@
+export const BASE_URL = "https://vufysay.nomorepartiesco.ru/api";
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+};
